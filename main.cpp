85a8e85f07bd27f332f6401e22b118026f80b4fc
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-05-31 00:44:32 -0300

    if statements

--- main.cpp
+++ main.cpp
@@ -15,7 +15,32 @@ int main()
     std::cout << "Enter two numbers: \a";
     std::cin >> num1 >> num2;
     std::cout << "You entered: " << num1 << " and " << num2 << std::endl;
-    std::cout << "The multiplication of " << num1 << " and " << num2 << " is " << num1 * num2 << std::endl;
-    std::cout << "The division of " << num1 << " and " << num2 << " = " << (float)num1 / num2;
+
+    std::cout << "The multiplication of "
+              << num1 << " and "
+              << num2 << " is "
+              << num1 * num2 << std::endl;
+
+    std::cout << "The division of "
+              << num1 << " and "
+              << num2 << " = "
+              << (float)num1 / num2 << std::endl;
+
+    if (num1 > num2)
+    {
+        std::cout << "\n"
+                  << num1 << " is greater than " << num2 << std::endl;
+    }
+    else if (num1 == num2)
+    {
+        std::cout << num1
+                  << " is equal to " << num2 << std::endl;
+    }
+    else
+    {
+    std:
+        cout << num1
+             << " is less than " << num2 << std::endl;
+    }
     return 0;
 }
