7f8e5d97d4f5cfb9539250aab0c89507c82d2daf
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-05-31 00:29:17 -0300

    reading two variables on terminal

--- main.cpp
+++ main.cpp
@@ -6,8 +6,14 @@ int main()
 {
     int val;
 
-    std::cout << "Enter a number: ";
+    std::cout << "Enter a number: \a";
     std::cin >> val;
     std::cout << "You entered: " << val << std::endl;
+
+    int num1, num2;
+
+    std::cout << "Enter two numbers: \a";
+    std::cin >> num1 >> num2;
+    std::cout << "You entered: " << num1 << " and " << num2 << std::endl;
     return 0;
 }
