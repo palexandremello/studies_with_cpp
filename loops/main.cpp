cb09f625851762d70004581aae42a49116d5ba70
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-06-01 01:47:35 -0300

    while

---
+++ loops/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+
+    int a = 0;
+
+    while (a < 10)
+    {
+        cout << a << endl;
+        a += 1;
+    }
+}
