1fdf837ba8c756b5bea7004fb543291808faf75e
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-06-08 11:11:10 -0300

    learning Generics Type on C++

--- main.cpp
+++ main.cpp
@@ -29,6 +29,26 @@ int Rectangle::perimeter() { return 2 * (len + bread); };
 
 Rectangle::~Rectangle() { cout << "Destructor called" << endl; };
 
+template <class T> class Arithmetic {
+private:
+  T a;
+  T b;
+
+public:
+  Arithmetic(T a, T b);
+  T add();
+  T sub();
+};
+
+template <class T> Arithmetic<T>::Arithmetic(T a, T b) {
+  this->a = a;
+  this->b = b;
+};
+
+template <class T> T Arithmetic<T>::add() { return a + b; };
+
+template <class T> T Arithmetic<T>::sub() { return a - b; };
+
 int main() {
   Rectangle r(10, 5);
 
@@ -36,5 +56,9 @@ int main() {
   cout << "Perimeter is " << r.perimeter() << endl;
   r.changeLength(20);
   cout << "Area is " << r.area() << endl;
+
+  Arithmetic<int> ar(10, 5);
+  cout << ar.add() << endl;
+
   return 0;
 }
