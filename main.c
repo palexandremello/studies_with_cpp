8aa567b679fb98fdd9cc2293880319f3c61ba975
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-06-14 01:55:11 -0300

    first power recursion

--- main.c
+++ main.c
@@ -22,9 +22,17 @@ int fact(int n) {
   }
   return fact(n - 1) * n;
 }
+
+int power(int m, int n) {
+  if (n == 0)
+    return 1;
+  return power(m, n - 1) * m;
+}
 int main() {
   int a = sum(5);
   int b = fact(5);
+  int c = power(2, 9);
   printf("%d ", a);
   printf("%d", b);
+  printf(" %d ", c);
 }
