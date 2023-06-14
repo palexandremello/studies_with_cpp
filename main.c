89f3478403b651a642b1b8c2c82a2e80441642d0
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-06-14 00:32:47 -0300

    nested recursion

--- main.c
+++ main.c
@@ -2,15 +2,14 @@
 #include <stdio.h>
 
 int fun(int n) {
-
-  if (n > 0) {
-    printf("%d ", n); // 1 2 3 (3 2 1
-    fun(n - 1);
-    fun(n - 1);
+  if (n > 100) {
+    return n - 10;
+  } else {
+    return fun(fun(n + 11));
   }
 }
 
 int main() {
-  printf("%d ", fun(3)); // fun(3
-  return 0;
+  int a = fun(100);
+  printf("%d", a);
 }
