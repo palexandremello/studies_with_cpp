ced6b1cbce8abec86521d8c48121344366b0375d
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-06-14 01:56:53 -0300

    power recursion using even and odds value to reduce multiplications

--- main.c
+++ main.c
@@ -28,11 +28,22 @@ int power(int m, int n) {
     return 1;
   return power(m, n - 1) * m;
 }
+
+int pow1(int m, int n) {
+  if (n == 0)
+    return 1;
+
+  if (n % 2 == 0)
+    return pow1(m * m, n / 2);
+  return m * pow1(m * m, (n - 1) / 2);
+}
 int main() {
   int a = sum(5);
   int b = fact(5);
   int c = power(2, 9);
+  int d = pow1(2, 9);
   printf("%d ", a);
   printf("%d", b);
   printf(" %d ", c);
+  printf(" %d ", d);
 }
