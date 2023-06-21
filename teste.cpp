0581a98a6ed8fc443cb59a289abfbe0ae9b81575
Paulo Alexandre Mello <palexandremello@gmail.com>
2023-06-20 22:05:40 -0300

    booleans

---
+++ teste.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::boolalpha;
+
+
+int main() {
+
+
+   cout << boolalpha;
+   cout << (true && true && true && true) << endl;
+   cout << (false  || true) << endl;
+
+   return 0;
+
+}
